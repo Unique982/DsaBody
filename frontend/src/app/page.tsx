2f1e4a25dd48fc9387ac2ef503c7be07804b2151
Unique Neupane <khemrajneupane111@gmail.com>
2025-11-29 08:53:24 +0545

    first commit

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-blue-500">Hello Tailwind!</h1>
+      <div className="bg-red-500 p-4 text-white">Tailwind Test</div>
+    </>
+  );
+}
